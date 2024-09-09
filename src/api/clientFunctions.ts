2011fd1008d8d79a7889207831bb9f1d99456be4
Ayush Abhishek Kumar <ayushabhishekkumar19@gmail.com>
2024-09-09 19:27:00 +0530

    feat: enhance API client functions with schema validation for depth and trades

--- src/api/clientFunctions.ts
+++ src/api/clientFunctions.ts
@@ -1,5 +1,12 @@
-import { Ticker, TickerSchema } from "../utils/schemas";
-import { Depth, KLine, Trade } from "../utils/types";
+import {
+  DepthSchema,
+  Depth,
+  Ticker,
+  TickerSchema,
+  Trade,
+  TradeSchema,
+} from "../utils/schemas";
+import { KLine } from "../utils/types";
 import axiosClient from "./axiosInstance";
 import ENDPOINTS from "./endpoints";
 
@@ -33,13 +40,16 @@ export const getTickers = async (): Promise<Ticker[]> => {
 export const getDepth = async (market: string): Promise<Depth> => {
   const response = await axiosClient({
     method: "GET",
-    url: ENDPOINTS.DEPTH,
-    params: {
-      symbol: market,
-    },
+    url: `${ENDPOINTS.DEPTH}/${market}`,
   });
 
-  return response.data;
+  const parsedDepthData = DepthSchema.safeParse(response.data);
+
+  if (!parsedDepthData.success) {
+    throw new Error("Invalid depth data");
+  }
+
+  return parsedDepthData.data;
 };
 
 export const getTrades = async (market: string): Promise<Trade[]> => {
@@ -51,7 +61,22 @@ export const getTrades = async (market: string): Promise<Trade[]> => {
     },
   });
 
-  return response.data;
+  if (!Array.isArray(response.data)) {
+    throw new Error("Invalid trade list");
+  }
+
+  const trades: Trade[] = [];
+  for (const trade of response.data) {
+    const parsedTrade = TradeSchema.safeParse(trade);
+
+    if (!parsedTrade.success) {
+      throw new Error("Invalid trade");
+    }
+
+    trades.push(parsedTrade.data);
+  }
+
+  return trades;
 };
 
 export const getKlines = async (
