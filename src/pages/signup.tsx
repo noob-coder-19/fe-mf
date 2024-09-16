9b5b3b0cc29973044b81bbbb5957a8b3aebeb4ed
Ayush Abhishek Kumar <ayushabhishekkumar19@gmail.com>
2024-09-16 19:24:00 +0530

    feat: added signup page without routes

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,5 +1,65 @@
+import { FormEvent } from "react";
+import { Link } from "react-router";
+
 const Signup = () => {
-  return <div>Sign up page</div>;
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const { email, password } = Object.fromEntries(
+      new FormData(e.currentTarget)
+    );
+
+    console.log(email, password);
+  };
+
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <div className="min-w-[400px] w-2/5 flex flex-col p-6 gap-4 rounded-xl bg-white/10">
+        <h1 className="text-center text-4xl">Register</h1>
+
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+          {/* Email */}
+          <input
+            autoComplete="email"
+            className="px-3 py-2 rounded-md"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+            required
+          />
+
+          {/* Password */}
+          <input
+            autoComplete="current-password"
+            className="px-3 py-2 rounded-md"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Password"
+            required
+          />
+
+          <span className="text-right text-sm">
+            Already have an account?{" "}
+            <Link
+              className="font-semibold text-ebony-400 hover:underline"
+              to="/login"
+            >
+              Login
+            </Link>
+          </span>
+
+          <button
+            className="flex-1 p-2 rounded-lg bg-white text-black"
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
