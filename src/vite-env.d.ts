492661bf6a7cb65895183ff90eeadd1b7a2a3b73
Ayush Abhishek Kumar <ayushabhishekkumar19@gmail.com>
2024-09-20 03:11:00 +0530

    feat: add VITE_CORE_MARKET environment variable to vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   readonly VITE_BACKEND_URL: string;
   readonly VITE_WEBSOCKET_URL: string;
   readonly VITE_INTERVAL: string;
+  readonly VITE_CORE_MARKET: string;
 }
 
 interface ImportMeta {
