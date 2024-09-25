667062ae21688caba27a1429254361dc6bd93a6b
Ayush Abhishek Kumar <ayushabhishekkumar19@gmail.com>
2024-09-26 01:22:00 +0530

    feat: add userId state and setter to store for user management

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,6 +3,8 @@ import { create } from "zustand";
 type Store = {
   accessToken: string | null;
   setAccessToken: (accessToken: string | null) => void;
+  userId: string | null;
+  setUserId: (userId: string | null) => void;
 };
 
 const useStore = create<Store>()((set) => ({
@@ -10,6 +12,8 @@ const useStore = create<Store>()((set) => ({
   // accessToken: "sf",
   setAccessToken: (newAccessToken) =>
     set((state) => ({ ...state, accessToken: newAccessToken })),
+  userId: null,
+  setUserId: (newUserId) => set((state) => ({ ...state, userId: newUserId })),
 }));
 
 export default useStore;
