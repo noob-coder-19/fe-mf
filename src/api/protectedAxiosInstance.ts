9a43415fd2408bc3224d9d2c8e22254aeea4be98
Ayush Abhishek Kumar <ayushabhishekkumar19@gmail.com>
2024-09-27 04:41:00 +0530

    feat: implement protectedAxiosClient with token management for API requests

---
+++ src/api/protectedAxiosInstance.ts
@@ -0,0 +1,61 @@
+import axios from "axios";
+import useStore from "../store";
+import { getAccessToken } from "./clientFunctions";
+
+const protectedAxiosClient = axios.create({
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  withCredentials: true,
+});
+
+protectedAxiosClient.interceptors.request.use(
+  async (config) => {
+    console.log(useStore.getState().accessToken);
+
+    const { accessToken, setAccessToken } = useStore.getState();
+
+    if (!accessToken) {
+      console.log("No access token found");
+      const newAccessToken = await getAccessToken();
+
+      console.log("New access token: ", newAccessToken);
+
+      if (newAccessToken) {
+        setAccessToken(newAccessToken);
+        config.headers.Authorization = `Bearer ${newAccessToken}`;
+        return config;
+      }
+    } else {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+protectedAxiosClient.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error?.response?.status === 401 && !originalRequest?._retry) {
+      originalRequest._retry = true;
+
+      const newAccessToken = await getAccessToken();
+
+      if (newAccessToken) {
+        useStore.getState().setAccessToken(newAccessToken);
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        return axios(originalRequest);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default protectedAxiosClient;
