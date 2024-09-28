38740afa00cba50ecc76531f5a0e1612c114a5af
Ayush Abhishek Kumar <ayushabhishekkumar19@gmail.com>
2024-09-28 06:09:00 +0530

    feat: refactor Balance component to accept balance as a prop and remove internal state management

--- src/components/trade/Balance.tsx
+++ src/components/trade/Balance.tsx
@@ -1,48 +1,15 @@
-import { useEffect, useState } from "react";
-
 type Props = {
+  balance: number;
   currency: string;
   buy: boolean;
 };
 
-type AvailableBalanceType = {
-  buy: number;
-  sell: number;
-};
-
 const Balance = (props: Props) => {
-  const [balance, setBalance] = useState<AvailableBalanceType>({
-    buy: 0.0,
-    sell: 0.0,
-  });
-
-  const currency = props.buy ? "USDC" : "SOL";
-  const availableBalance = props.buy ? balance.buy : balance.sell;
-
-  useEffect(() => {
-    const fetchBalance = async (): Promise<AvailableBalanceType> => {
-      return new Promise((resolve) => {
-        setTimeout(
-          () =>
-            resolve({
-              buy: 103.0001,
-              sell: 2.034751,
-            }),
-          1000
-        );
-      });
-    };
-
-    fetchBalance().then((balance) => {
-      setBalance(balance);
-    });
-  }, []);
-
   return (
     <div className="flex flex-row text-xs items-center justify-between">
       <p className="text-gray-400">Available Balance:</p>
       <p className="font-semibold">
-        {availableBalance} {currency}
+        {props.balance} {props.currency}
       </p>
     </div>
   );
