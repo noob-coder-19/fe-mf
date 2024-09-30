97d8307cf5b69403d5ee277e42f3c12544755502
Ayush Abhishek Kumar <ayushabhishekkumar19@gmail.com>
2024-09-30 20:42:00 +0530

    feat: add refresh button to Balance component with loading state

--- src/components/trade/Balance.tsx
+++ src/components/trade/Balance.tsx
@@ -1,16 +1,35 @@
+import classNames from "classnames";
+import { ICONS } from "../../utils/icons";
+
 type Props = {
   balance: number;
   currency: string;
   buy: boolean;
+  loading: boolean;
+  refreshBalance: () => void;
 };
 
 const Balance = (props: Props) => {
   return (
     <div className="flex flex-row text-xs items-center justify-between">
       <p className="text-gray-400">Available Balance:</p>
-      <p className="font-semibold">
-        {props.balance} {props.currency}
-      </p>
+
+      <div className="flex flex-row items-center gap-2">
+        <p className="font-semibold">
+          {props.balance} {props.currency}
+        </p>
+
+        <button
+          disabled={props.loading}
+          onClick={() => {
+            console.log("refreshing balance");
+            props.refreshBalance();
+          }}
+          className={classNames(`${ICONS.loading}`, {
+            "animate-spin": props.loading,
+          })}
+        ></button>
+      </div>
     </div>
   );
 };
